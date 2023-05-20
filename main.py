89e935daebcae2115c5d1c9ad8b81b072369d93d
Holdmedve <talos.boti@gmail.com>
2023-05-20 10:42:06 +0200

    ♻️

--- main.py
+++ main.py
@@ -7,23 +7,38 @@ from bs4 import BeautifulSoup
 
 openai.api = os.getenv("OPENAI_API_KEY")
 
-def main():
-    cv = PdfReader('botond_talos_cv.pdf')
-    number_of_pages = len(cv.pages)
-    page = cv.pages[0]
-    cv_text = page.extract_text()
+def extract_cv_text(cv_path):
+    cv = PdfReader(cv_path)
+    return cv.pages[0].extract_text()
 
-    URL = "https://www.linkedin.com/jobs/view/3608802118/?alternateChannel=search&refId=72Guo3os4OXMRrw71tpcbg%3D%3D&trackingId=6oKe455c%2BlwD7NXfx4MRXA%3D%3D"
-    page = requests.get(URL)
+
+def scrape_linkedin_job_description(url):
+    page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
-    job_description = soup.find('div', class_='description__text description__text--rich').getText()
-    # print(job_description)
+    return soup.find('div', class_='description__text description__text--rich').getText()
 
-    prompt = f'write a cover letter for this job: {job_description}. This is my CV: {cv_text}'
-    response = openai.Completion.create(model='text-davinci-003', prompt=prompt, temperature=0.6, n=1, max_tokens=1000)
-    cover_letter = response['choices'][0]['text']
-    # print(cover_letter)
-    print(response)
+
+def generate_cover_letter(job_description, cv):
+    prompt = f'write a cover letter for this job: {job_description}. This is my CV: {cv}'
+    response = openai.Completion.create(
+        model='text-davinci-003', 
+        prompt=prompt, 
+        max_tokens=1000,
+        temperature=0.6, 
+        n=1, 
+    )
+    return response['choices'][0]['text']
+
+
+def main():
+    cv = extract_cv_text(cv_path='botond_talos_cv.pdf')
+    job_description = scrape_linkedin_job_description(
+        url="https://www.linkedin.com/jobs/view/3608802118/?alternateChannel=search&refId=72Guo3os4OXMRrw71tpcbg%3D%3D&trackingId=6oKe455c%2BlwD7NXfx4MRXA%3D%3D"
+    )
+    cover_letter = generate_cover_letter(job_description=job_description, cv=cv)
+    print(cover_letter)
+    
+    
 
 
 if __name__ == '__main__':
